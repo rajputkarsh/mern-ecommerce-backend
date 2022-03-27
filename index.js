de957f8a03db774ef692e78bdc73473c726feb21
Utkarsh <r.utkarsh.0010@gmail.com>
2022-03-27 21:27:59 +0530

    Product CRUD Operations
    Added CRUD operations for Products model

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ const dotenv   = require("dotenv")
 
 const userRoutes = require("./routes/user")
 const authRoutes = require("./routes/authentication")
+const productRoutes = require("./routes/product")
 
 dotenv.config()
 const app = express()
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URL)
-    .then(() => {
-        console.log("Database Connected")
-    })
-    .catch((error)=>{
-        console.log("ERROR while connecting database", error)
-    })
+.then(() => {
+    console.log("Database Connected")
+})
+.catch((error)=>{
+    console.log("ERROR while connecting database", error)
+})
 
-app.use("/api/user", userRoutes)
 app.use("/api/auth", authRoutes)
+app.use("/api/user", userRoutes)
+app.use("/api/products", productRoutes)
 
 app.listen(process.env.PORT, ()=>{
     console.log("BACKEND SERVER")
