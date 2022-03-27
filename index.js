b207f98b37e65b3e0318fdf39d32b040626d34d9
Utkarsh <r.utkarsh.0010@gmail.com>
2022-03-27 22:19:22 +0530

    Added Stripe Payment
    Added Stripe payment APIs for checkout and payment confirmation

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express  = require("express")
 const mongoose = require("mongoose")
+var cors       = require('cors')
 const dotenv   = require("dotenv")
-
-const userRoutes = require("./routes/user")
-const authRoutes = require("./routes/authentication")
-const productRoutes = require("./routes/product")
-const cartRoutes = require("./routes/cart")
-const orderRoutes = require("./routes/order")
 
 dotenv.config()
 const app = express()
 app.use(express.json())
+app.use(cors())
+
+const userRoutes    = require("./routes/user")
+const authRoutes    = require("./routes/authentication")
+const productRoutes = require("./routes/product")
+const cartRoutes    = require("./routes/cart")
+const orderRoutes   = require("./routes/order")
+const stripeRoutes  = require("./routes/stripe")
+
 
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
@@ -25,6 +29,7 @@ app.use("/api/user",     userRoutes)
 app.use("/api/products", productRoutes)
 app.use("/api/cart",     cartRoutes)
 app.use("/api/order",    orderRoutes)
+app.use("/api/checkout", stripeRoutes)
 
 app.listen(process.env.PORT, ()=>{
     console.log("BACKEND SERVER")
